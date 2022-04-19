33b1c9813d8cbf941be2b090b6a6afec7526f3ee
Ackim Brian <kimbrian2010@gmail.com>
2022-04-19 02:55:17 +0200

    Resizing of uploaded Images

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#from PIL import Image
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -11,5 +11,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    #def save(self):
-       # super().save()
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 200 or img.width > 200:
+            output_size = (200, 200)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
