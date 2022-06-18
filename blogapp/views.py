c6332a7407dd65686282a5af8008aa8b7f0a29b7
Ackim Brian <kimbrian2010@gmail.com>
2022-06-18 19:02:04 +0200

    created index page - as start page

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 
+def index(request):
+    return render(request, 'blogapp/index.html')
+
+@login_required()
 def home(request):
     context = {
         'posts': Post.objects.all()
