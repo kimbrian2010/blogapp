0437f88bc88af7db886c0c95b0286dcb4b17a312
Ackim Brian <kimbrian2010@gmail.com>
2022-07-23 22:45:41 +0200

    Added user posts page

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='blog-home'),
+    path('userpost/', views.user_profile_post, name='user-post'),
     path('about/', views.about, name='about'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.contrib.auth.decorators import login_required
 
 
+
 def index(request):
     return render(request, 'blogapp/index.html')
 
@@ -12,6 +13,13 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blogapp/home.html', context)
+
+
+def user_profile_post(request):
+    context = {
+        'posts': Post.objects.filter(author=request.user)
+    }
+    return render(request, 'blogapp/user_post.html', context)
 
 
 def about(request):
